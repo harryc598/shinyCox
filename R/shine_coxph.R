4e675850b573a557a7042d49ae17ef0993454691
Harrison Clement <harryc598@gmail.com>
2023-07-13 16:37:33 -0500

    shinydashboard compatibality, more documentation

--- R/shine_coxph.R
+++ R/shine_coxph.R
@@ -5,6 +5,10 @@
 #'
 #' @param ... Any number of coxph models
 #' @param app.dir directory where shiny app will be written
+#' @param theme theme of shiny app. This changes the overall appearance of the
+#'   app. The first is the default theme, requiring only shiny to use. The
+#'   "dashboard" theme requires the shinydashboard and DT packages to run
+#'   properly.
 #' @section Details:
 #'
 #'   One of the important features of this function is that the shiny app, once
@@ -18,6 +22,11 @@
 #'   included in the package that will handle these requirements and provide
 #'   warnings if your model as constructed is unlikely to work with this
 #'   function.
+#'
+#'   There are some more guidelines for formula notation. There is some leniency
+#'   in that you can use functions inside the formula, but using nested
+#'   functions will fail. Considering the input names used in shiny are taken
+#'   directly from the formula, it is best not to add too many convolutions.
 #'
 #'
 #' @returns A shiny app written into specified directory.
@@ -42,31 +51,32 @@
 #' @export
 #' @importFrom grDevices hcl.colors
 #' @importFrom shiny showTab
-shine_coxph=function(..., app.dir = NULL)
+shine_coxph <- function(..., app.dir = NULL, theme = c("default", "dashboard"))
 
 {
   ########################
   # determine the class of each input argument
-  input.list=list(...)
-  n.list=length(input.list)
-  list.class=rep("",n.list)
+  input.list <- list(...)
+  n.list <- length(input.list)
+  list.class <- rep("",n.list)
   for (i in 1:n.list) {
-    list.class[i]=class(input.list[[i]])
+    list.class[i] <- class(input.list[[i]])
   }
-
+## ERROR for CLASS coxms, coxph.penal
   #######################
   # extract the coxph models from the input
-  cox.list=which(list.class=="coxph")
-  n.model=length(cox.list)
-  model.list=vector("list",n.model)
+  cox.list <- which(list.class == "coxph")
+  n.model <- length(cox.list)
+  model.list <- vector("list",n.model)
   for (i in 1:n.model) {
-    model.list[[i]]=input.list[[cox.list[i]]]
+    model.list[[i]] <- input.list[[cox.list[i]]]
   }
-  names(model.list)=names(input.list)[cox.list]
+  names(model.list) <- names(input.list)[cox.list]
 
   #######################
   # get colors list
   # RColorBrewer brewer.pal
+  # I don't think this is necessary anymore
   clrs=input.list$clrs
   if (is.null(clrs)) {
     clrs=hcl.colors(n.model, "Dark 2", alpha = 1)
@@ -76,44 +86,50 @@ shine_coxph=function(..., app.dir = NULL)
   # get app directory
   #app.dir=input.list$app.dir
   if (is.null(app.dir)) {
-    app.dir=getwd()
+    app.dir <- getwd()
   }
-  date.time=as.character(Sys.time())
-  date.time=gsub(":","-",date.time,fixed=TRUE)
-  date.time=gsub(" ","-",date.time,fixed=TRUE)
-  app.dir=paste0(app.dir,"/",date.time,"/")
+  date.time <- as.character(Sys.time())
+  date.time <- gsub(":", "-", date.time, fixed=TRUE)
+  date.time <- gsub(" ", "-", date.time, fixed=TRUE)
+  app.dir <- paste0(app.dir, "/", date.time, "/")
   dir.create(app.dir)
-  message(paste0("Shiny app will be written in directory ",app.dir,"."))
+  message(paste0("Shiny app will be written in directory ", app.dir, "."))
 
   #######################
   # convert the coxph models to coxfit objects
-  coxfit.list=vector("list",n.model)
+  coxfit.list <- vector("list",n.model)
   for (i in 1:n.model) {
-    coxfit.list[[i]]=prep_coxfit(model.list[[i]])
+    coxfit.list[[i]] <- prep_coxfit(model.list[[i]])
   }
-  names(coxfit.list)=names(model.list)
+  names(coxfit.list) <- names(model.list)
 
   #######################
   # Save app data into app directory
-  cox.fit.list=coxfit.list
-  save(cox.fit.list,clrs,file=paste0(app.dir,"appData.Rdata"))
+  cox.fit.list <- coxfit.list
+  save(cox.fit.list, clrs, file = paste0(app.dir, "appData.Rdata"))
 
   #######################
   # get different code sections
-  input.data.code=write_coxfit_input_data_code(coxfit.list)
-  KM.plot.code=write_KM_plot_code(coxfit.list,clrs)
-  table.code=prop_haz_tables(cox.fit.list)
+  input.data.code <- write_coxfit_input_data_code(coxfit.list)
+  KM.plot.code <- write_KM_plot_code(coxfit.list, clrs)
+  theme <- match.arg(theme)
+  if (theme == "default") {
+  table.code <- prop_haz_tables(cox.fit.list)
+  } else if (theme == "dashboard") {
+     table.code <- make_DT_table(cox.fit.list)
+   } else stop("Invalid theme, please choose 'default' or 'dashboard' as a theme")
 
   ########################
   # generate header code
-  hdr.code=c("#rm(list=ls())",
+  hdr.code <- c("#rm(list=ls())",
              "options(stringsAsFactors=FALSE)",
-             paste0("load('",gsub("\\\\", "/", app.dir),"appData.Rdata')"))
+             paste0("load('", gsub("\\\\", "/", app.dir), "appData.Rdata')"))
 
 
   ########################
   # generate ui code
-  ui.code=c("ui=navbarPage(",
+  if (theme == "default") {
+  ui.code <- c("ui=navbarPage(",
             "             'Cox Model Survival Predictions',",
             "    tabPanel('Plot',",
             "             sidebarLayout(",
@@ -139,16 +155,50 @@ shine_coxph=function(..., app.dir = NULL)
             "tableOutput(outputId = 'cox.times')))),",# SUBODH ADDITION
             "tabPanel('Summary Tables',",
             "mainPanel(",
-            # "h3('Hazard Ratio Summary Table'),", # SUBODH ADDITION
-            # "tableOutput(outputId = 'HR'),", # SUBODH ADDITION
-            # "h3('Assessing the Proportional Hazards Assumption'),", # SUBODH ADDITION
-            # "tableOutput(outputId = 'PHA')", # SUBODH ADDITION
             table.code$ui.code, ### NEW
             ")))") # SUBODH CHANGED THE FLOW
+  } else if (theme == "dashboard") {
+    ui.code <- c("library(shinydashboard)",
+                     "ui = dashboardPage(",
+                     "           dashboardHeader(title = 'Cox Model Survival Predictions'),",
+                     "           dashboardSidebar(",
+                     "             sidebarMenu(",
+                     "             menuItem('Plot', tabName = 'plot'),",
+                     table.code$uitop,
+                     "             )",
+                     "           ),",
+                     "     dashboardBody(",
+                     "       tabItems(",
+                     "     tabItem(tabName = 'plot',",
+                     "             sidebarLayout(",
+                     "             sidebarPanel(",
+                     paste0("                          ",
+                            input.data.code$ui.code,
+                            ","),
+                     "textInput('predProbTimes','Times for predicted probabilities',placeholder='Enter values separated by a comma'),", # SUBODH ADDITION
+                     "actionButton(inputId = 'go', label = 'Generate Plot'),",
+                     "actionButton(inputId = 'reset', label = 'Reset'),", # SUBODH ADDITION
+                     "actionButton(inputId = 'app.exit', label = 'Exit App'),",
+                     "selectInput('clrs', label = 'Choose Colors', choices = palette.pals(), selected = 'Okabe-Ito'),", # color choice
+                     "hr(),",
+                     "numericInput('height', 'Plot Height', value = 100),",
+                     "numericInput('width', 'Plot Width', value = 100),",
+                     "downloadButton('downloadPlot', 'Download Plot')",
+                     "),",
+                     "mainPanel(",
+                     "h3('Predicted Survival Curve'),",
+                     "plotOutput(outputId = 'KM'),",
+                     "h3('Predicted Probability at Fixed Times'),", # SUBODH ADDITION
+                     "textOutput(outputId='noPredTimes'),", # SUBODH ADDITION
+                     "tableOutput(outputId = 'cox.times')))),",
+                     table.code$uibottom,
+                     "))))"
+    )
+  }
 
   #########################
   # generate server code
-  server.code=c("server=function(input,output)",
+  server.code <- c("server = function(input,output)",
                 "{",
                 "          observeEvent(input$app.exit, {stopApp()}) # Exit when exit button is pressed",
                 "          observeEvent(input$go, {",
@@ -158,15 +208,15 @@ shine_coxph=function(..., app.dir = NULL)
                 table.code$server.code, ### NEW
                 "predProbTable <- cox_times_table(KM.hat,input$predProbTimes)", # SUBODH ADDITION
                 "if (is.null(predProbTable)) output$noPredTimes <- renderText('No input times detected. If you provided times, check that you separated numbers with a single comma and you provided valid numbers.') else output$noPredTimes <- renderText(invisible())", # SUBODH ADDITION
-                "output$cox.times=renderTable(predProbTable,rownames=TRUE)", # SUBODH ADDITION
-                "output$HR=renderTable(cox.fit.list[[1]]$HR.table,rownames=TRUE)", # SUBODH ADDITION
-                "output$PHA=renderTable(cox.fit.list[[1]]$PHA.table$table,rownames=TRUE)", # SUBODH ADDITION
-                "colors = palette.colors(length(cox.fit.list), input$clrs)", # colors
+                "output$cox.times <- renderTable(predProbTable, rownames = TRUE)", # SUBODH ADDITION
+                "output$HR <- renderTable(cox.fit.list[[1]]$HR.table, rownames = TRUE)", # SUBODH ADDITION
+                "output$PHA <- renderTable(cox.fit.list[[1]]$PHA.table$table, rownames = TRUE)", # SUBODH ADDITION
+                "colors <- palette.colors(length(cox.fit.list), input$clrs)", # colors
                 "output$downloadPlot <- downloadHandler(",
                 "  filename = function() { paste0('plot.png') },",
                 "  content = function(file) {",
                 "    png(file, input$width, input$height)",
-                "    cox.KM.plots(KM.hat,clrs=colors)",
+                "    cox.KM.plots(KM.hat, clrs = colors)",
                 "    dev.off()",
                 "  })",
                 "})",
@@ -175,29 +225,29 @@ shine_coxph=function(..., app.dir = NULL)
 
   ##########################
   # app code
-  app.code=c("cox.app=shinyApp(ui,server)")
+  app.code <- c("cox.app = shinyApp(ui,server)")
              #"runApp(cox.app)")
 
   ###########################
   # write the app file
-  app.code.file=paste0(app.dir,"app.R")
-  write(unlist(hdr.code),file=app.code.file,sep="\n")
-  write(unlist(ui.code),file=app.code.file,sep="\n",append=TRUE)
-  write(unlist(server.code),file=app.code.file,sep="\n",append=TRUE)
-  write(unlist(app.code),file=app.code.file,sep="\n",append=TRUE)
+  app.code.file <- paste0(app.dir, "app.R")
+  write(unlist(hdr.code), file = app.code.file, sep = "\n")
+  write(unlist(ui.code), file = app.code.file, sep = "\n", append = TRUE)
+  write(unlist(server.code), file = app.code.file, sep = "\n", append = TRUE)
+  write(unlist(app.code), file = app.code.file, sep = "\n", append = TRUE)
 
 
 
   ##########################
   # return result
 
-  res=list(app.dir=app.dir,
-           app.data.file=paste0(app.dir,"/appData.Rdata"),
-           app.code.file=app.code.file,
-           coxfit.list=coxfit.list,
-           hdr.code=hdr.code,
-           ui.code=ui.code,
-           server.code=server.code)
+  res=list(app.dir = app.dir,
+           app.data.file = paste0(app.dir, "/appData.Rdata"),
+           app.code.file = app.code.file,
+           coxfit.list = coxfit.list,
+           hdr.code = hdr.code,
+           ui.code = ui.code,
+           server.code = server.code)
 
   return(res)
 
