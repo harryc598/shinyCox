f62195e641cd3122ae5fbd055d4bfa16ffee31a9
Harrison Clement <harryc598@gmail.com>
2023-07-17 15:52:47 -0500

    Removed unused functions

--- R/shiny_blocks.R
+++ R/shiny_blocks.R
@@ -65,63 +65,6 @@ ez_slider=function(vname,  # variable name
 }
 
 
-###################################################################################
-# Generate ui and server code for an action button;
-# embeds server.lines into the action of the button
-# NOT USED IN CODE
-ez_button=function(id,              # identifier of the button
-                   label,           # label to show on the button
-                   server.lines="") # lines to be executed in server when button is pushed
-
-{
-  ui.code=paste0("actionButton(inputId = '",id,
-                 "', label = '",label,"')")
-  server.code=c(paste0('observeEvent(input$',id,', {'),
-                server.lines,'})')
-  server.code=gsub('{"','{',server.code,fixed=T)
-  res=list(ui.code=ui.code,
-           server.code=server.code)
-  return(res)
-}
-
-
-###################################
-# Generate an exit app button
-# NOT USED IN CODE
-ez_exit=function(id="app.exit",
-                 label="Exit App")
-{
-  exit.code="stopApp()"
-  res=ez_button(id,label,exit.code)
-  res$server.code=paste0(res$server.code,collapse="")
-  res$server.code=paste0(res$server.code,"# Exit when exit button is pressed")
-  return(res)
-}
-
-####################################
-# Create a sidebar panel layout
-# NOT USED IN CODE
-ez_sidebarLayout=function(ui.sidebar,  # ui code for the sidebar
-                          ui.main)     # ui code for the main panel
-{
-  # Add commas after all but the last line of ui.sidebar
-  n.sidebar=length(ui.sidebar)
-  ui.sidebar[-n.sidebar]=paste0(ui.sidebar[-n.sidebar],",")
-
-  # Add some indention space to enhance readability
-  ui.sidebar=paste0("    ",ui.sidebar)
-
-  # similar operations for main panel ui code
-  n.main=length(ui.main)
-  ui.main[-n.main]=paste0(ui.main[-n.main],",")
-  ui.main=paste0("    ",ui.main)
-
-  # write the code before and after
-  res=c("sidebarLayout(",
-        "  sidebarPanel(",ui.sidebar,"    ), ",
-        "  mainPanel(",ui.main,"    ))")
-  return(res)
-}
 #' Generate code for `selectInput()`
 #'
 #' Creates a `selectInput()` function in shiny ui and
